5071509ef29b58115244e417e922197a1ae3cd88
Wing Kei Leung <winky.leung@gmail.com>
2017-08-01 03:31:55 +0000

    Added header

--- avatarDownloaded/download_avatars.js
+++ avatarDownloaded/download_avatars.js
@@ -1,24 +1,26 @@
 var request = require('request');
 var fs = require('fs');
+var newBody = "";
 
 var GITHUB_USER = "wingkeileung"
-var GITHUB_TOKEN = "95f23002c8c37a6c0d003a396db47ee326476562;"
+var GITHUB_TOKEN = "95f23002c8c37a6c0d003a396db47ee326476562"
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-
 var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+var options = {
+  url: requestURL,
+  headers: {"User-Agent": "GitHub Avatar Downloader - Student project"}
+}
 
-request.get(requestURL)
-  .on('error', function (err) {
-      throw err
-    console.log("Errors:", err);
-  })
-  .on('response', function (result) {
-    console.log("Result:", result);
-    console.log(requestURL);
-  });
+request(options, function(err, respnose, body) {
+  newBody = JSON.parse(body)
+  cb(err, newBody)
+  console.log(newBody)
+})
+
 }
 getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log(newBody)
 });
